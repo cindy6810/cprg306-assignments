3e5f4936d4a669a78e7f5a76d5d752c7ee46bff6
cindy6810 <167484719+cindy6810@users.noreply.github.com>
2024-09-23 18:29:07 -0600

    completed week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Yaling Wei</p>
+      <Link href="https://github.com/YOUR_GITHUB_USERNAME/cprg306-assignments">
+        GitHub Repository
+      </Link>
+    </div>
+  );
+}
