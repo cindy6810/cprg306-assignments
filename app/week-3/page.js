03d01b001c428bbd47eb60c348e55196cde4e47e
cindy6810 <167484719+cindy6810@users.noreply.github.com>
2024-10-01 15:21:42 -0600

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ import ItemList from "./item-list";
 const Page = () => {
   return (
     <main className="p-6 bg-black min-h-screen text-white">
-      <h1 className="text-3xl font-bold mb-8 text-center">Shopping List</h1>
+      <h1 className="text-3xl font-bold mb-8 ">Shopping List</h1>
       <ItemList />
     </main>
   );
